299364651783b47cb0f5cebc7a734f519eaf0827
6MillionBees <BoettcherAr321@northwested.org>
2024-10-02 11:06:12 -0400

    Update caesar-cipher-main.py

--- caesar-cipher-main.py
+++ caesar-cipher-main.py
@@ -1,10 +1,11 @@
 # Arden Boettcher
 # 10/2/24
 # Caesar Cipher
+# This isn't for the assignment I just wanted to see if I could do it
 
 words = input('Enter text: ')
 offset = int(input('Enter offset: '))
-while offset >= 26:
+while offset >= 26: 
     offset -= 26
 
 for letter in words.lower():
